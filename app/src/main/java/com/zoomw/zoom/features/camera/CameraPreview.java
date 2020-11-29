804f2e80d04d3171286edc4905666d338d575964
kimchaeyeon <jessicakcy0501@gmail.com>
2020-11-29 10:33:41 +0900

    initial commit

--- app/src/main/java/com/zoomw/zoom/features/camera/CameraPreview.java
+++ app/src/main/java/com/zoomw/zoom/features/camera/CameraPreview.java
@@ -1,7 +1,7 @@
 package com.zoomw.zoom.features.camera;
 
 import android.content.Context;
-import android.graphics.Camera;
+import android.hardware.Camera;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -27,6 +27,25 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             this.camera.setPreviewDisplay(holder);
             this.camera.startPreview();
         } catch(IOException e){
+            Log.d("CameraPreview", "미리보기 생성 실패: "+e.getMessage());
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height){
+        if (this.holder.getSurface() == null){
+            return;
+        }
+        try{
+            this.camera.stopPreview();
+        }catch (Exception e){
+
+        }
+
+        try{
+            this.camera.setPreviewDisplay(this.holder);
+            this.camera.startPreview();
+        } catch (Exception e){
             Log.d("CameraPreview", "미리보기 생성 실패: "+e.getMessage());
         }
     }
